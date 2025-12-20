f392d583aa93e401b3f8f5f53e599e9895b46469
Altusha4 <240782@astanait.edu.kz>
2025-12-20 17:10:05 +0600

    Render country info, exchange rates and news headlines

--- public/app.js
+++ public/app.js
@@ -9,9 +9,9 @@ async function generateUser() {
     const res = await fetch('/api/user');
     const data = await res.json();
 
-    const { user, country, exchange } = data;
+    const { user, country, exchange, news } = data;
 
-    // ---------- Exchange HTML ----------
+    // ---------- Exchange ----------
     let exchangeHTML = '<p><b>Exchange Rates:</b> N/A</p>';
 
     if (exchange && country.currencyCode) {
@@ -19,6 +19,25 @@ async function generateUser() {
             <h3>Exchange Rates</h3>
             <p>1 ${country.currencyCode} = ${exchange.USD.toFixed(2)} USD</p>
             <p>1 ${country.currencyCode} = ${exchange.KZT.toFixed(2)} KZT</p>
+        `;
+    }
+
+    // ---------- News ----------
+    let newsHTML = '<p><i>No news available</i></p>';
+
+    if (news && news.length > 0) {
+        newsHTML = `
+            <h3>Latest News about ${user.country}</h3>
+            <div class="news-list">
+                ${news.map(n => `
+                    <div class="news-card">
+                        ${n.image ? `<img src="${n.image}" alt="News image">` : ''}
+                        <h4>${n.title}</h4>
+                        <p>${n.description}</p>
+                        <a href="${n.url}" target="_blank">Read full article</a>
+                    </div>
+                `).join('')}
+            </div>
         `;
     }
 
@@ -34,20 +53,17 @@ async function generateUser() {
             <p><b>Country:</b> ${user.country}</p>
             <p><b>Address:</b> ${user.address}</p>
 
-            ${
-        country
-            ? `
-                        <hr>
-                        <h3>Country Info</h3>
-                        <p><b>Capital:</b> ${country.capital}</p>
-                        <p><b>Languages:</b> ${country.languages}</p>
-                        <p><b>Currency:</b> ${country.currency}</p>
-                        <img src="${country.flag}" width="120"><br><br>
+            <hr>
+            <h3>Country Info</h3>
+            <p><b>Capital:</b> ${country.capital}</p>
+            <p><b>Languages:</b> ${country.languages}</p>
+            <p><b>Currency:</b> ${country.currency}</p>
+            <img src="${country.flag}" width="120"><br><br>
 
-                        ${exchangeHTML}
-                      `
-            : `<p><i>Country data temporarily unavailable</i></p>`
-    }
+            ${exchangeHTML}
+
+            <hr>
+            ${newsHTML}
         </div>
     `;
 }
